26d5ce67756910ea52efb9f7fede6c94d801b6a9
Alex Khrizman <akhrizman@gmail.com>
2022-07-20 00:53:19 -0400

    testing flask endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from BgDbUpdaterService import BgDbUpdaterService
 
 app = Flask(__name__)   # Flask constructor
@@ -7,6 +7,24 @@ app = Flask(__name__)   # Flask constructor
 @app.route('/')
 def hello():
     return 'BG Database Updater'
+
+
+@app.route('/get', methods=['GET'])
+def test_get():
+    if request.method == 'GET':
+        data = {
+            "testGet": "Get Works",
+        }
+        return jsonify(data)
+
+
+@app.route('/post', methods=['POST'])
+def test_post():
+    if request.method == 'POST':
+        data = {
+            "testPost": "Post Works",
+        }
+        return jsonify(data)
 
 
 @app.route('/syncAll/', defaults={'update_recent': 'skip'})
@@ -25,7 +43,7 @@ def sync_all_games(update_recent):
         # Syncing All Except Recently Synced
         bg_updater_service.update_all_games(skip_recently_modified=True)
 
-    return "Sync Complete"
+    return 'Sync Complete'
 
 
 @app.route('/testSync')
@@ -37,7 +55,7 @@ def test_sync_one_game():
         return 'Database is Locked. Try again later'
     else:
         bg_updater_service.test_update()
-    return "Test Sync Complete"
+    return 'Test Sync Complete'
 
 
 @app.route('/syncNew')
@@ -50,28 +68,28 @@ def sync_new_games():
         return 'Database is Locked. Try again later'
 
     bg_updater_service.update_new_games()
-    return "Sync Complete"
+    return 'Sync Complete'
 
 
 @app.route('/lockStatus')
 def check_lock_status():
     bg_updater_service = BgDbUpdaterService()
     locked = bg_updater_service.get_lock_status()
-    return "LOCKED" if locked else "UNLOCKED"
+    return 'LOCKED' if locked else 'UNLOCKED'
 
 
 @app.route('/unlock')
 def unlock_database():
     bg_updater_service = BgDbUpdaterService()
     bg_updater_service.unlock_database()
-    return "UNLOCKED"
+    return 'UNLOCKED'
 
 
 @app.route('/lock')
 def lock_database():
     bg_updater_service = BgDbUpdaterService()
     bg_updater_service.lock_database()
-    return "LOCKED"
+    return 'LOCKED'
 
 
 if __name__ == '__main__':
