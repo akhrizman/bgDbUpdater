db019cbf1e847845d3e7b35dd53431b9c78ebbbf
Alex Khrizman <akhrizman@gmail.com>
2022-08-01 19:10:22 -0400

    new code to link to spring app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
 from BgDbUpdaterService import BgDbUpdaterService
 import os
 
@@ -10,53 +10,30 @@ def hello():
     return 'BG Database Updater'
 
 
-@app.route('/get', methods=['GET'])
-def test_get():
-    if request.method == 'GET':
-        data = {
-            "testGet": "Get Works",
-        }
-        return jsonify(data)
-
-
-@app.route('/post', methods=['POST'])
-def test_post():
-    if request.method == 'POST':
-        data = {
-            "testPost": "Post Works",
-        }
-        return jsonify(data)
-
-
-@app.route('/syncAll/', defaults={'update_recent': 'skip'}, methods=['GET'])
-@app.route('/syncAll/<update_recent>', methods=['GET'])
-def sync_all_games(update_recent):
+@app.route('/syncAll', methods=['GET'])
+def sync_all_games():
+    print('Attempting to Update All Games')
     bg_updater_service = BgDbUpdaterService()
     locked = bg_updater_service.get_lock_status()
 
     if locked:
-        return 'Database is Locked. Try again later'
+        return app.response_class(json.dumps('Database is Locked. Try again later'), content_type='application/json')
 
-    if update_recent == 'force':
-        # Syncing All
-        bg_updater_service.update_all_games(skip_recently_modified=False)
-    else:
-        # Syncing All Except Recently Synced
-        bg_updater_service.update_all_games(skip_recently_modified=True)
-
-    return 'Sync Complete'
+    bg_updater_service.update_all_games(skip_recently_modified=True)
+    return app.response_class(json.dumps('Sync Complete'), content_type='application/json')
 
 
-@app.route('/testSync', methods=['GET'])
-def test_sync_one_game():
+@app.route('/syncAllIncludingRecent', methods=['GET'])
+def sync_all_games_including_recent():
+    print('Attempting to Update All Games including recently updated')
     bg_updater_service = BgDbUpdaterService()
     locked = bg_updater_service.get_lock_status()
 
     if locked:
-        return 'Database is Locked. Try again later'
-    else:
-        bg_updater_service.test_update()
-    return 'Test Sync Complete'
+        return app.response_class(json.dumps('Database is Locked. Try again later'), content_type='application/json')
+
+    bg_updater_service.update_all_games(skip_recently_modified=False)
+    return app.response_class(json.dumps('Sync Complete'), content_type='application/json')
 
 
 @app.route('/syncNew', methods=['GET'])
@@ -67,31 +44,43 @@ def sync_new_games():
     locked = bg_updater_service.get_lock_status()
 
     if locked:
-        return 'Database is Locked. Try again later'
+        return app.response_class(json.dumps('Database is Locked. Try again later'), content_type='application/json')
 
     bg_updater_service.update_new_games()
-    return 'Sync Complete'
+    return app.response_class(json.dumps('Sync Complete'), content_type='application/json')
+
+
+@app.route('/testSync', methods=['GET'])
+def test_sync_one_game():
+    bg_updater_service = BgDbUpdaterService()
+    locked = bg_updater_service.get_lock_status()
+
+    if locked:
+        return app.response_class(json.dumps('Database is Locked. Try again later'), content_type='application/json')
+    else:
+        bg_updater_service.test_update()
+    return app.response_class(json.dumps('Test Sync Complete'), content_type='application/json')
 
 
 @app.route('/lockStatus', methods=['GET'])
 def check_lock_status():
     bg_updater_service = BgDbUpdaterService()
-    locked = bg_updater_service.get_lock_status()
-    return 'LOCKED' if locked else 'UNLOCKED'
+    is_locked = bg_updater_service.get_lock_status()
+    return app.response_class(json.dumps(is_locked), content_type='application/json')
 
 
 @app.route('/unlock', methods=['POST'])
 def unlock_database():
     bg_updater_service = BgDbUpdaterService()
     bg_updater_service.unlock_database()
-    return 'UNLOCKED'
+    return app.response_class(json.dumps("UNLOCKED"), content_type='application/json')
 
 
 @app.route('/lock', methods=['POST'])
 def lock_database():
     bg_updater_service = BgDbUpdaterService()
     bg_updater_service.lock_database()
-    return 'LOCKED'
+    return app.response_class(json.dumps("LOCKED"), content_type='application/json')
 
 
 if __name__ == "__main__":
