0576e5e75e62ea46c4107fb03983e1407a4f88e5
Alex Khrizman <a.khrizman@gmail.com>
2022-08-03 23:57:47 -0400

    .gitignore updated

--- appconfigs/dev_settings.py
+++
@@ -1,7 +0,0 @@
-DATASOURCE = {
-  'user': 'pmgapp',
-  'password': 'No@h2017Ell@2020',
-  'host': '192.168.1.4',
-  'database': 'boardgamedb_dev',
-  'raise_on_warnings': True
-}
--- appconfigs/prod_settings.py
+++
@@ -1,7 +0,0 @@
-DATASOURCE = {
-  'user': 'pmgapp',
-  'password': 'No@h2017Ell@2020',
-  'host': '192.168.1.4',
-  'database': 'boardgamedb',
-  'raise_on_warnings': True
-}
